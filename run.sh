51e6ce098531b4613d774cc1f0c3a11a241189e2
ShenChen <shenchen2296@163.com>
2025-12-01 04:00:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+export TZ='Asia/Shanghai'
 git ls-files --directory ./blog/source | while read path; do
     touch -d "$(git log -1 --format='@%ct' $path)" "$path"; 
     echo "$path: $(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
