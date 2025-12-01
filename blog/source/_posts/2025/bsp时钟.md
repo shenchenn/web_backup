6d0248b46c384d02cdc75a18094fd0703bba12c6
ShenChen <shenchen2296@163.com>
2025-12-02 00:11:40 +0800

    1

--- blog/source/_posts/2025/bsp时钟.md
+++ blog/source/_posts/2025/bsp时钟.md
@@ -1,5 +1,6 @@
 ---
 title: 时钟
+date: {{ date }}
 categories:
   - 嵌入式
 tags:
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 export TZ='Asia/Shanghai'
-# git ls-files --directory ./blog/source | while read path; do
-#     touch -d "$(git log -1 --format='@%ct' $path)" "$path"; 
-#     echo "$path: $(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
-# done
+git ls-files --directory ./blog/source | while read path; do
+    touch -d "$(git log -1 --format='@%ct' $path)" "$path"; 
+    echo "$path: $(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
+done
